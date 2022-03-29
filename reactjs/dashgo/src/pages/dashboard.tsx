c34f56c55ba6e6b3c62e2d9411535e10f885081d
Ana Carolina Gomes <anagomes@positivosolucoes.com.br>
2022-03-28 22:58:54 -0300

    feat(dashgo): implementa estrutura da sidebar

--- reactjs/dashgo/src/pages/dashboard.tsx
+++ reactjs/dashgo/src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import { Flex } from '@chakra-ui/react';
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
 export default function Dashboard() {
   return (
-    <Header />
-  )
-};
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" maxW={1480} my="6" mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
+}
