3e813c686154e5c863ded4171e8227132430db18
Ana Carolina Gomes <anagomes@positivosolucoes.com.br>
2022-03-29 16:33:38 -0300

    feat(dashgo): implementa responsividade do Header

--- reactjs/dashgo/src/components/Header/Profile.tsx
+++ reactjs/dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Ana Gomes</Text>
-        <Text color="gray.300" fontSize="small">
-          gomesanac@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Ana Gomes</Text>
+          <Text color="gray.300" fontSize="small">
+            gomesanac@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
