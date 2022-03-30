9193c53711d9a37aad52dde10675771bfef86535
Ana Carolina Gomes <anagomes@positivosolucoes.com.br>
2022-03-30 19:12:52 -0300

    feat(dashgo): implementa navegação pelo links e botões

--- reactjs/dashgo/src/pages/users/create.tsx
+++ reactjs/dashgo/src/pages/users/create.tsx
@@ -8,8 +8,9 @@ import {
   SimpleGrid,
   VStack,
 } from '@chakra-ui/react';
-import { Input } from '../../components/Form/Input';
+import Link from 'next/link';
 
+import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 
@@ -46,7 +47,9 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button colorScheme="whiteAlpha">Cancelar</Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
