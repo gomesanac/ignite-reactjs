268e22a65afd159e3cfdd2a81336200a16cfb7d8
Ana Carolina Gomes <anagomes@positivosolucoes.com.br>
2022-04-18 16:34:52 -0300

    feat(dashgo): confugr react query devtools

--- reactjs/dashgo/src/pages/_app.tsx
+++ reactjs/dashgo/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
@@ -20,6 +21,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
