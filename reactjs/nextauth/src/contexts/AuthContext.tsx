9b1d715efe200d3cac59ceec28f6fb4a6a7e4bfc
Ana Carolina Gomes <anagomes@positivosolucoes.com.br>
2022-05-02 19:11:20 -0300

    feat(nextauth): recupera token, busca dados do usuário e seta header de autorização

--- reactjs/nextauth/src/contexts/AuthContext.tsx
+++ reactjs/nextauth/src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState } from 'react';
-import { setCookie } from 'nookies';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { setCookie, parseCookies } from 'nookies';
 import { useRouter } from 'next/router';
 
 import { api } from '../services/api';
@@ -33,6 +33,23 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies();
+
+    if (token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({
+          email,
+          permissions,
+          roles,
+        });
+      })
+    }
+
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post('sessions', { email, password });
@@ -54,6 +71,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         permissions,
         roles,
       });
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
       router.push('/dashboard');
     } catch (error) {
