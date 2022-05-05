3c9797596a1660d667f0bfc7846130d259d743ed
Ana Carolina Gomes <anagomes@positivosolucoes.com.br>
2022-05-05 09:23:55 -0300

    feat(nextauth): implemente componente de permissão

--- reactjs/nextauth/src/pages/dashboard.tsx
+++ reactjs/nextauth/src/pages/dashboard.tsx
@@ -1,17 +1,15 @@
 import { useContext, useEffect } from 'react';
 import type { NextPage } from 'next';
 
+import { Can } from '../components/Can';
 import { AuthContext } from '../contexts/AuthContext';
+import { setupAPIClient } from '../services/api';
 import { api } from '../services/apiClient';
 import styles from '../styles/Home.module.css';
 import { withSSRAuth } from '../utils/withSSRAuth';
-import { setupAPIClient } from '../services/api';
-import { useCan } from '../hooks/useCan';
 
 const Dashboard: NextPage = () => {
   const { user } = useContext(AuthContext);
-
-  const useCanSeeMetrics = useCan({ permissions: ['metrics.list'] });
 
   useEffect(() => {
     api
@@ -25,7 +23,9 @@ const Dashboard: NextPage = () => {
   return (
     <div className={styles.container}>
       <h1>Dashboard: {user?.email}</h1>
-      {useCanSeeMetrics && <div>Métricas</div>}
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
     </div>
   );
 };
